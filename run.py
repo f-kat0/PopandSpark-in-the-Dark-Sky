9d9d4a9ccc61179368e4305cd10b757c25429177
f-kat0 <kamonporn.tod@ku.th>
2024-12-16 09:09:01 +0700

    change the default gameplay

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from game import Game
 
 # The player is able to adjust the number of balls and time limit here
-num_balls = 10
-time_limit = 60
+num_balls = 15
+time_limit = 30
 
 game = Game(num_balls=num_balls, time_limit=time_limit)
 game.run()
